37ec29e8fba569c34176dd8dfeb5698eb2176dc2
Comealongwithme <comealongwithme@protonmail.com>
2023-01-23 02:07:54 +0600

    feat: baseUrl for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const baseURL = process.env.NODE_ENV === 'production' ? '/bronevik/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: baseURL
 })
